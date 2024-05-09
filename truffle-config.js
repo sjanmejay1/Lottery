2043e45ef2283067341b5751b442bcc830cde9a2
Janmejay <sjanmejay1@gmail.com>
2024-05-09 23:53:57 +0530

    ignored files

--- truffle-config.js
+++ truffle-config.js
@@ -58,9 +58,9 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    goerli: {
-    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    network_id: 5,       // goerli's id
+    sepolia: {
+    provider: () => new HDWalletProvider(mnemonic, `https://eth-sepolia.g.alchemy.com/v2/XK2WQZQqZn5bKHSeIOHzqVzjj6Z0CKUp`),
+    network_id: 11155111,       // sepolia's id
     gas: 5500000,        // Ropsten has a lower block limit than mainnet
     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
